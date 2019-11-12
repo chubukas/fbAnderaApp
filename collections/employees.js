630976c6afdba3093460d43742da68e768b387c9
ebuka <chubukass@gmail.com>
2019-11-12 03:18:32 +0100

    added table schema

--- collections/employees.js
+++
@@ -1,9 +0,0 @@
-const query = `INSERT INTO employees VAUES (id serial PRIMARY KEY NOT NULL,
-    firstName ,
-    lastName ,
-    email ,
-    password ,
-    gender ,
-    jobRole ,
-    department ,
-    address )`;
