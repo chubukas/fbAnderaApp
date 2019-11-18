d02c567de1d40d4c108df1fedf7aae71437d2ad6
ebuka <chubukass@gmail.com>
2019-11-18 13:54:39 +0100

    added travis Ci

---
+++ db/db.js
@@ -0,0 +1,68 @@
+const { Pool } = require("pg");
+const tables = require("./tables");
+
+const dbCreate = () => {
+  try {
+    const pool = new Pool({
+      connectionString: "postgres://postgres:student55@127.0.0.1:5432/teamwork"
+    });
+
+    pool
+      .connect()
+      .then(() => {
+        console.log("connected to database");
+
+        // Connecting the tables
+        pool
+          .query(tables.userTable)
+          .then(() => {
+            console.log("userTable created successfully");
+          })
+          .catch(err => {
+            console.log(`${err}`);
+          });
+
+        pool
+          .query(tables.gifTable)
+          .then(() => {
+            console.log("gifTable created successfully");
+          })
+          .catch(err => {
+            console.log(err);
+          });
+        pool
+          .query(tables.gifCommentTable)
+          .then(() => {
+            console.log("gifCommentTable created successfully");
+          })
+          .catch(err => {
+            console.log(err);
+          });
+
+        pool
+          .query(tables.articleTable)
+          .then(() => {
+            console.log("articleTable created successfully");
+          })
+          .catch(err => {
+            console.log(err);
+          });
+
+        pool
+          .query(tables.articleCommentTable)
+          .then(() => {
+            console.log("articleCommentTable created successfully");
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  } catch (error) {
+    console.log(`This is the catch error ${error} `);
+  }
+};
+
+module.exports = dbCreate;
---
+++ db/tables.js
@@ -0,0 +1,51 @@
+// Create User Tables
+exports.userTable = `CREATE TABLE IF NOT EXISTS
+      employees(
+        id serial PRIMARY KEY NOT NULL,
+        firstName VARCHAR(255) NOT NULL,
+        lastName VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        gender VARCHAR(255) NOT NULL,
+        jobRole VARCHAR(255) NOT NULL,
+        department VARCHAR(255) NOT NULL,
+        address VARCHAR(255) NOT NULL
+      )`;
+
+exports.gifTable = `CREATE TABLE IF NOT EXISTS
+      gifs(
+        id serial PRIMARY KEY NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        postedBy VARCHAR(255),
+        createdOn TIMESTAMP
+      )`;
+
+exports.gifCommentTable = `CREATE TABLE IF NOT EXISTS
+        gifComment(
+        id serial PRIMARY KEY NOT NULL,
+        comment VARCHAR(255555) NOT NULL,
+        gifID INTEGER NOT NULL,
+        postedBy VARCHAR(255),
+        createdOn TIMESTAMP,
+        FOREIGN KEY (gifID) REFERENCES gifs (id) on delete cascade on update cascade
+      )`;
+
+exports.articleTable = `CREATE TABLE IF NOT EXISTS
+      article(
+        id serial PRIMARY KEY NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        article VARCHAR(255555) NOT NULL,
+        postedBy VARCHAR(255),
+        createdOn TIMESTAMP
+      )`;
+
+exports.articleCommentTable = `CREATE TABLE IF NOT EXISTS
+      articleComment(
+        id serial PRIMARY KEY NOT NULL,
+        comment VARCHAR(255555) NOT NULL,
+        articleID INTEGER NOT NULL,
+        postedBy VARCHAR(255),
+        createdOn TIMESTAMP,
+        FOREIGN KEY (articleID) REFERENCES article (id) on delete cascade on update cascade
+      )`;
